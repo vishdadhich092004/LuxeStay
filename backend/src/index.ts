8ce74168309b858d8481532fb83ed203be1c1279
Vishesh Dadhich <visheshdadhich20@gmail.com>
2024-07-24 12:05:52 +0530

    Added '*' route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,6 +45,12 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
+
+// non defined routes
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.listen(7000, () => {
   console.log("PORT ACTIVATED 7000");
 });
